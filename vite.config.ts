12364b71bd6c96c026e5a9ba596c4c61e7479803
Christopher Tonnesland <christopher.tonnesland@redgo.no>
2024-12-13 07:50:20 +0100

    Setting up vite settings and a dev branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+ base: "/wiki/",
+ plugins: [react()],
+ resolve: {
+  alias: {
+   "@": path.resolve(__dirname, "src"),
   },
-  server: {
-    port: 5235, 
-  },
-  build: {
-    target: 'esnext',
-    outDir: 'public', 
-    minify: 'esbuild',
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'], 
-  },
-})
+ },
+ server: {
+  port: 5235,
+ },
+ build: {
+  target: "esnext",
+  outDir: "dist",
+  minify: "esbuild",
+ },
+ optimizeDeps: {
+  include: ["react", "react-dom", "react-router-dom"],
+ },
+});
